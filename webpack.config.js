66604d911cc785e6698604ae0176d6a3fc10c4b7
Matthew Orndoff <mattheworndoff@gmail.com>
2016-08-05 21:48:23 -0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+// Webpack configuration
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: 'build',
+        filename: 'bundle.js'
+    },
+    module: {
+
+        loaders: [{
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            loader: 'babel',
+            query: {
+              presets: ['es2015']
+            }
+        }]
+    }
+};
