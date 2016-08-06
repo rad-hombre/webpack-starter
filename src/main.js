be29d3736104deb4f4bd92039d69a2b23f0ef953
Matthew Orndoff <mattheworndoff@gmail.com>
2016-08-06 13:13:45 -0700

    Add SASS watchin'

--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
-
+require('./style.scss');
 const $ = require('jquery');
-$('#target').html("Hi there budddy, dummy!");
+$('#target').html("Hi there dummy!");
--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,17 @@ module.exports = {
             exclude: /(node_modules)/,
             loader: 'babel',
             query: {
-              presets: ['es2015']
+                presets: ['es2015']
             }
+        },
+        {
+            test: /\.scss$/,   // does this search entire rootDir?
+            loader: 'style-loader!css-loader!sass-loader'
         }]
+    },
+    devServer: {
+        inline: true,
+        contentBase: './build',
+        port: 3000
     }
 };
